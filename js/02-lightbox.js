f9af422eed3d665ee95c7c10bddf07b71a994a8f
Alexey Brazhnik <alexey.brazhnyk@gmail.com>
2022-07-21 22:04:03 +0300

    задание 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,33 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryRef = document.querySelector('.gallery');
+const galleryMarkup = createGalleryMarkup(galleryItems);
+
+galleryRef.innerHTML = galleryMarkup;
+
+galleryRef.addEventListener('click', onGalleryItemClick);
+
+const gallery = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
+
+function onGalleryItemClick(e) {
+  e.preventDefault();
+
+  if (e.target.nodeName !== 'IMG') return;
+}
+
+function createGalleryMarkup(items) {
+  return items
+    .map(
+      ({ preview, original, description }) =>
+        `<a class="gallery__item" href="${original}">
+          <img class="gallery__image" src="${preview}" 
+          alt="${description}" />
+        </a>`
+    )
+    .join('');
+}
